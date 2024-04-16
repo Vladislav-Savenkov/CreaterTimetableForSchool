36c1061af786fa942a8963204f5c3412a712cb07
Vladislav-Savenkov <61520327+Vladislav-Savenkov@users.noreply.github.com>
2024-04-17 01:38:44 +0500

    part 3 of N

---
+++ LessonsFolder/AddLesson.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FuckingSchoolProject.LessonsFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для AddLesson.xaml
+    /// </summary>
+    public partial class AddLesson : Window
+    {
+        public Lesson Lesson { get; private set; }
+        public AddLesson(Lesson l)
+        {
+            InitializeComponent();
+            Lesson = l;
+            this.DataContext = Lesson;
+        }
+
+        private void Click_AcceptLesson(object sender, RoutedEventArgs e)
+        {
+            if (TBoxLessonName.Text == "")
+            {
+                MessageBox.Show("Поле \"Название предмета\" не может быть пустым!", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+                this.DialogResult = true;
+        }
+    }
+}
---
+++ LessonsFolder/Lesson.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FuckingSchoolProject
+{
+    public class Lesson :INotifyPropertyChanged
+    {
+        private string name;
+        private string teachersNames;
+        public int LessonID { get; set; }
+        public string LessonName
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("LessonName");
+            }
+        }
+        public string TeachersNames
+        {
+            get { return teachersNames; }
+            set
+            {
+                teachersNames = value;
+                OnPropertyChanged("TeachersNames");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop="")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
